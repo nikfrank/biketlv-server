d4802a8db897b39411029d3c865a327e6c768ac1
nik frank <nikbfrank@gmail.com>
2018-10-13 15:50:30 -0700

    starting deployment

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 const cors = require('cors');
 
+console.log(process.env);
 
 const sequelize = new Sequelize('biketlv', 'biketlv', 'nikisgreat', {
   host: 'localhost',
@@ -18,17 +19,14 @@ const sequelize = new Sequelize('biketlv', 'biketlv', 'nikisgreat', {
   }
 });
 
-sequelize.authenticate()
-         .then(() => {
-           console.log('connection has been established successfully.');
-         })
-         .catch(err => {
-           console.error('Unable to connect to the database:', err);
-         });
+sequelize
+  .authenticate()
+  .then(() => console.log('connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err));
 
 const app = express();
 
-const BikeLane = sequelize.define('bikelane', {
+const Lane = sequelize.define('bikelane', {
   startLat: Sequelize.FLOAT,
   startLng: Sequelize.FLOAT,
   endLat: Sequelize.FLOAT,
@@ -46,41 +44,47 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.get('/home', (req, res) => {
-  console.log('people')
-  res.json({people:[ 'tom','tim','timmy',' timmie' ]})
-});
-
-app.get('/bikelane', (req, res) => {
-    BikeLane.findAll({})
-            .then(rows => {
-              res.json(rows)
-            })
-            .catch(err => res.status(err.code || 500).json({ err }))
+app.get('/lane', (req, res) => {
+  Lane.findAll({})
+      .then(rows => res.json(rows))
+      .catch(err => res.status(err.code || 500).json({ err }))
   
 });
 
-app.post('/bikelane', (req, res) => {
-    BikeLane.create(req.body.payload)
-            .then(createdRow => {
-              res.json(createdRow)
-            })
-         .catch(err => res.status(err.code || 500).json({ err }))
-
+app.post('/lane', (req, res) => {
+  Lane.create(req.body.payload)
+      .then(createdRow => res.json(createdRow))
+      .catch(err => res.status(err.code || 500).json({ err }))
 });
+
+app.get('/lane/deprecate/:id', (req, res) => {
+  Lane.destroy({ where: { id: 1* req.params.id  } })
+      .then((a) => res.json({ message: a }))
+      .catch(err => res.status(err.code || 500).json({ err }))
+});
+
+app.put('/lane/:id', (req, res) =>
+  Lane.findById(1 * req.params.id)
+      .then(row =>
+        (row ?
+         row.update(req.body.payload)
+            .then(patchedRow => res.json({
+              schema: 'lane',
+              payload: [patchedRow],
+            })) :
+         Promise.reject('lane with id ' + (req.params.id) + ' not found')
+        ) )
+      .catch(err => res.status(err.code || 500).json({ err }))
+);
 
 
 app.get('/hydrate', (req, res) => {
-  BikeLane.sync({force: true})
-          .then(() => {
-    res.json({})
-  });
+  Lane.sync({force: true})
+      .then(() => res.json({}));
   
 });
 
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+app.get('*', (req, res) => res.status(200).sendfile('./build/'+req.path));
 
 
 module.exports = app;
