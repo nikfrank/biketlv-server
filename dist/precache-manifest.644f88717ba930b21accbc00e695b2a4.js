90f238ba2e4314cb256eb7ca83a8ff11d0d24fde
nik frank <nikbfrank@gmail.com>
2018-10-13 16:15:50 -0700

    deploy

---
+++ dist/precache-manifest.644f88717ba930b21accbc00e695b2a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2dc8eaf7f6744e3e6edd",
+    "url": "/static/js/main.2dc8eaf7.chunk.js"
+  },
+  {
+    "revision": "68954b5eec02b3af7585",
+    "url": "/static/js/1.68954b5e.chunk.js"
+  },
+  {
+    "revision": "2dc8eaf7f6744e3e6edd",
+    "url": "/static/css/main.8cc1d3ad.chunk.css"
+  },
+  {
+    "revision": "68954b5eec02b3af7585",
+    "url": "/static/css/1.0f22df28.chunk.css"
+  },
+  {
+    "revision": "b69a716db7b72a03065d0d8dec9e7906",
+    "url": "/index.html"
+  }
+];
